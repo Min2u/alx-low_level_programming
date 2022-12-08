3b7a8f0ef325adfc40e5d6c3d5ea9ddb2ff10a02
Min2u <mintea561065@gmail.com>
2022-12-08 12:13:12 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * Return: Always 0
  */
 int main(void)
-
-	puts("\"Programming is like building a multi lingual puzzle");
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
+}
