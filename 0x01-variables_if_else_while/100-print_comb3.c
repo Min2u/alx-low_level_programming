95bb78b6cc58b7f96042acdd2473cb3c38bb2b67
Min2u <mintea561065@gmail.com>
2022-12-10 06:44:10 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,19 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - main block
- * Discription: Write a progaram that prints all possible
- * Numbers must be separated by ,, followed by a space
- * The two digits must be different
- * 01 and 10 are considered the same combination of the two digits 0 and 1
- * Print only the smallest combination of two digits
- * Numbers should be printed in ascending order, with two digits
- * You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
- * You can only use putchar five times maximum in your code
- * You are not allowed to use any variable of type char
- * All your code should be in the main function
- * Retern: 0
+ * Retern: Always 0
  */
 int main(void)
 {
@@ -36,7 +25,6 @@ int main(void)
 					putchar(' ');
 				}
 			}
-
 			c++;
 		}
 		d++;
