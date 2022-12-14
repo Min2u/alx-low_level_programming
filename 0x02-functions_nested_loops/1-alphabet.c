4dc0613fe048c95236ac187811314fccb04cad64
Min2u <mintea561065@gmail.com>
2022-12-14 12:32:38 -0800

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print - alphabet - Makke the alphabet
+ * print_alphabet - Make the alphabet
  * Return: void
  */
 
@@ -12,7 +12,7 @@ void print_alphabet(void)
 	
 	for (c = 'a'; c <= 'z'; c++)
 	{
-        _putchar(c);
+	_putchar(c);
 	}
 	_putchar ('\n');
 }
