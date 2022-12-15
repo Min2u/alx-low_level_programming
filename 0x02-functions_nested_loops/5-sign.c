370569a92c0a7d8805e599684933d443fb6d9f34
Min2u <mintea561065@gmail.com>
2022-12-15 12:25:49 -0800

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  * @c: The number to be checked.
- * Return: 1 for negative num or 0 for anything else
+ * Return: 1 for negative num or zero for anything else
  */
 
 int print_sign(int n)
