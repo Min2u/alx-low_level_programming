177d5dbddf401a9e66ee82b5cf3aa1978ce4a7bc
Min2u <mintea561065@gmail.com>
2022-12-20 13:08:24 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,9 @@
-#ifindef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(cchar *s);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
