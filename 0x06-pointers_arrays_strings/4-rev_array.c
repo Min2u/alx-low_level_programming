b959ac96caee8c5c0820b1b5a8f50d193672ed92
Min2u <mintea561065@gmail.com>
2022-12-23 04:54:00 -0800

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,7 +3,7 @@
 /**
  * reverse_array - function that reverse content of array
  * @a: array
- * @n: number of elements of the array
+ * @n: number of elements
  */
 
 void reverse_array(int *a, int n)
@@ -13,6 +13,7 @@ void reverse_array(int *a, int n)
 	for (index = n - 1; index > n / 2; index--)
 	{
 		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
 		a[index] = tmp;
 	}
 }
