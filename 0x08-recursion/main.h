d7b66fa7350721df532f4a01281cd068b92dc69f
Min2u <mintea561065@gmail.com>
2023-01-04 14:52:57 -0800

    0-puts_recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+int _putchar(char c);
+void _puts_recursion(char*s);
+void _print-rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recrusion(int x, int y);
+int _sqrt_recuris(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
