29fcf76f753a5853f63949cad32a5b9e4b4e4091
Min2u <mintea561065@gmail.com>
2023-01-05 11:38:55 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,28 +1,24 @@
+#include "main.h"
 
-
-
-#include"main.h"
-
-/**_puts_recursion - prints a string followed by a linew line
- *
- *  @s: pointer to string
- *
+/**
+ * _puts_recursion - prints a string followed by a linew line
+ * @s: pointer to string
  *
  * Return: void
  *
  */
 
 void _puts_recursion(char *s)
-
 {
-	if (*s == '\0') /* base case */
-
+	if (*s == '\0') 
 	{
-	_putchar('\n');
-	return;
+		_putchar('\n');
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
 
 
