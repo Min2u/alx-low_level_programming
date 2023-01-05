7399648fd6a8cdc0d7de310559d182e6aeeba2b0
Min2u <mintea561065@gmail.com>
2023-01-05 11:57:37 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _prints_rev_reursion - prints a string, followed by a new line
+ * _prints_rev_recursion - prints a string, followed by a new line
  * @s: pointer to the string
  * Return: void
  */
