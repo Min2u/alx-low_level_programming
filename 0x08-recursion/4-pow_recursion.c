8a09235f22c55960c9cb6a0406a492de0a164b9f
Min2u <mintea561065@gmail.com>
2023-01-05 12:48:12 -0800

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _Pow_recursion - search a string for any of a set of bytes
+ * _pow_recursion - search a string for any of a set of bytes
  * @x: base
  * @y: exposant
  * Return: pointer to the byte in 's' that matchas one of the bytes in 'accept'
