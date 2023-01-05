93a05ae3a2eafed438b0be8251873cbca94b3f77
Min2u <mintea561065@gmail.com>
2023-01-05 11:45:57 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,6 +2,7 @@
 
 /**
  * _puts_recursion - prints a string followed by a linew line
+ *
  * @s: pointer to string
  *
  * Return: void
@@ -10,7 +11,7 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0') 
+	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
@@ -20,8 +21,3 @@ void _puts_recursion(char *s)
 		_puts_recursion(s + 1);
 	}
 }
-
-
-
-
-
